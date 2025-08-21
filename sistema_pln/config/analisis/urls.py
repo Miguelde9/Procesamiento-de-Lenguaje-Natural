5de96d79e57ee4c8053abda3b8f7dd34d9fcecd7
Miguel <migueldelgadomtz77@gmail.com>
2025-08-21 02:09:36 -0600

    Ajuste de templates y views en la app análisis - lista.html agregado, listas.html eliminado - Modificación en subir.html y views.py - Actualización en settings y base de datos

--- sistema_pln/config/analisis/urls.py
+++ sistema_pln/config/analisis/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('subir/', views.subir_texto, name='subir_texto'),
     path('', views.lista_textos, name='lista_textos'),
-    ]
+    path('analizar/<int:texto_id>/', views.analizar_texto, name='analizar_texto'),
+]
